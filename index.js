0c28214b5f8b74370928a7325b8b8cc0820f7458
Danda <ldanda@ttu.edu>
2023-02-13 12:20:48 -0600

    Added schemas for DB

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express()
 const dotenv = require("dotenv");
 const connectDB = require('./config/db');
-const userRoute = require('./routes/users');
+const userRoute = require('./routes/user');
 
 dotenv.config();
 
 // connect DB
 connectDB();
 
+app.use(express.json());
 //route
-app.use('/api/user', userRoute);
+app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log(`Our server is running at ${process.env.PORT}`);
--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,4 @@
 const router = require('express').Router();
 
-router.get("/usertest", (req,res)=>{
-    res.send("User tested")
-})
 
 module.exports = router;
