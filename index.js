893e4452422880c7aef0e8c67a8b2a9d2323bf8d
lingaiahdanda <lingiahdanda@gmail.com>
2023-02-13 16:34:35 -0600

    Added the product routes

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const dotenv = require("dotenv");
 const connectDB = require('./config/db');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
 dotenv.config();
 
 // connect DB
 connectDB();
 
 app.use(express.json());
-//route
+
+//routes
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log(`Our server is running at ${process.env.PORT}`);
