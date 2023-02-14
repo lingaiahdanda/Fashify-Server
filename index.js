5c28d17ccaf5eced604be007a2304a09ad421ac5
lingaiahdanda <lingaiahdanda@gmail.com>
2023-02-13 20:47:40 -0600

    Added stripe API for payment

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
-
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 dotenv.config();
 
 // connect DB
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 //routes
@@ -21,6 +23,8 @@ app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/cart', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout',stripeRoute);
+
 app.listen(process.env.PORT || 5000, ()=>{
     console.log(`Our server is running at ${process.env.PORT}`);
 })
